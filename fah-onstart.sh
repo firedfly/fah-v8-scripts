ada1f755446a40f57f610e34b610200c043f25a2
firedfly <firedfly@gmail.com>
2024-12-18 16:06:59 -0500

    install fah v8 and dependencies.

--- fah-onstart.sh
+++ fah-onstart.sh
@@ -1,2 +1,23 @@
 # onstart script for Folding@Home v8 running on vast.ai
 
+  echo "**** install runtime packages ****" && \
+  apt-get update && \
+  apt-get install -y \
+    bzip2 \
+    intel-opencl-icd \
+    libexpat1 \
+    screen && \
+  ln -s libOpenCL.so.1 /usr/lib/x86_64-linux-gnu/libOpenCL.so && \
+  echo "**** install foldingathome ****" && \
+  download_url="https://download.foldingathome.org/releases/public/fah-client/debian-10-64bit/release/fah-client_8.3.18-64bit-release.tar.bz2" && \
+  curl -o \
+    /tmp/fah.tar.bz2 -L \
+    ${download_url} && \
+  tar xf /tmp/fah.tar.bz2 -C /root --strip-components=1 && \
+  echo "**** cleanup ****" && \
+  apt-get clean && \
+  rm -rf \
+    /tmp/* \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /var/log/*

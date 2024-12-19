019b44e225a088552609aa7d0ce0ed4f912ae30b
firedfly <4018878+firedfly@users.noreply.github.com>
2024-12-19 05:10:38 -0500

    Initial auto start implementation (#1)
    * install fah v8 and dependencies.
    
    * Install pipx and lufah.  ensure fah log directory exists
    
    * start screen detached so it can launch on docker start
    
    * Use lufah to configure fah for folding on GPUs only
    
    * sleep 10 seconds after starting fah-client before configuring with lufah
    
    * Add gpus to the default fah group instead of creating a new group.  CPUs are disabled again when starting fah-client
    
    * start folding after configuring fah

--- fah-onstart.sh
+++ fah-onstart.sh
@@ -1,2 +1,33 @@
 # onstart script for Folding@Home v8 running on vast.ai
 
+  echo "**** install runtime packages ****" && \
+  apt-get update && \
+  apt-get install -y \
+    bzip2 \
+    intel-opencl-icd \
+    libexpat1 \
+    screen \
+    pipx && \
+  pipx install lufah && \
+   mkdir /var/log/fah-client && \
+  ln -s libOpenCL.so.1 /usr/lib/x86_64-linux-gnu/libOpenCL.so && \
+  echo "**** install foldingathome ****" && \
+  download_url="https://download.foldingathome.org/releases/public/fah-client/debian-10-64bit/release/fah-client_8.3.18-64bit-release.tar.bz2" && \
+  curl -o \
+    /tmp/fah.tar.bz2 -L \
+    ${download_url} && \
+  tar xf /tmp/fah.tar.bz2 -C /root --strip-components=1 && \
+  echo "**** cleanup ****" && \
+  apt-get clean && \
+  rm -rf \
+    /tmp/* \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /var/log/*
+
+screen -dm ./fah-client --log=/var/log/fah-client/log.txt --log-rotate-dir=/var/log/fah-client/ --account-token=$ACCOUNT_TOKEN --machine-name="Vast.ai-$VAST_CONTAINERLABEL" --cpus 0
+
+sleep 10
+
+.local/bin/lufah -a / enable-all-gpus
+.local/bin/lufah -a / fold

38030cd94d4fef88a3192692b25bb7456d53ff0f
firedfly <4018878+firedfly@users.noreply.github.com>
2025-01-24 12:51:47 -0500

    Fixes for instance specific settings (#3)
    * Read user, team, passkey after setting the values to see if they were actually set.  Part of debugging
    
    * A loop now runs constantly to ensure the specified user, team and passkey stay applied to the fah-client.  Since we
    
    are linking the client to an fah account, the account settings will sometimes overwrite our manually supplied values.
    
    Also, before we begin folding, we'll check again to make sure teh config settings were successfully applied.
    
    * Added missing qutation marks when comparing current and expected config values
    
    * When autostart delays due to config mismatch from specified settings, output the current values.
    
    * Team values are not strings, so no need to wrap in quotes to compare requested and current values
    
    * export all variables to the main shell so we can see what the script sees.
    
    * Autostart will now begin folding (again).  Added some extra logging to separate the log messages better.

--- fah-onstart.sh
+++ fah-onstart.sh
@@ -27,9 +27,12 @@
 
 FAH_MACHINE_NAME="Vast.ai-$VAST_CONTAINERLABEL"
 if [[ -v FAH_USERNAME ]]; then
-    echo "FAH username specified; Adding to machine name"
+    echo "FAH username specified; Updating machine name"
     FAH_MACHINE_NAME=$FAH_MACHINE_NAME-$FAH_USERNAME
 fi
+
+# make the current environment variables available to a standard shell 
+env >> /etc/environment;
 
 echo "Starting fah-clinent"
 screen -dm ./fah-client --log=/var/log/fah-client/log.txt --log-rotate-dir=/var/log/fah-client/ --account-token=$FAH_ACCOUNT_TOKEN --machine-name=$FAH_MACHINE_NAME --cpus 0
@@ -40,24 +43,82 @@ sleep 10
 echo "Enabling all GPUs"
 .local/bin/lufah -a / enable-all-gpus
 
-if [[ -v FAH_USERNAME ]]; then
-    echo "FAH username specified.  Updating FAH config"
-    .local/bin/lufah -a / config user $FAH_USERNAME    
-fi
+echo "===================================="
+echo "Beginning FAH configuration loop"
+while :
+do
+    if [[ -v FAH_USERNAME ]]; then
+        FAH_CURRENT_USERNAME=$(.local/bin/lufah -a / config user)
+        if [[ $FAH_CURRENT_USERNAME != "\"$FAH_USERNAME\"" ]]
+        then
+            echo "FAH username specified.  Updating FAH config"
+            .local/bin/lufah -a / config user $FAH_USERNAME
+            echo "---"
+        fi
+    fi
 
-if [[ -v FAH_TEAM ]]; then
-    echo "FAH team specified.  Updating FAH config"
-    .local/bin/lufah -a / config team $FAH_TEAM
-fi
+    if [[ -v FAH_TEAM ]]; then
+        FAH_CURRENT_TEAM=$(.local/bin/lufah -a / config team)
+        if [[ $FAH_CURRENT_TEAM != "$FAH_TEAM" ]]
+        then
+            echo "FAH team specified.  Updating FAH config"
+            .local/bin/lufah -a / config team $FAH_TEAM
+            echo "---"
+        fi
+    fi
 
-if [[ -v FAH_PASSKEY ]]; then
-    echo "FAH passkey specified.  Updating FAH config"
-    .local/bin/lufah -a / config passkey $FAH_PASSKEY    
-fi
+    if [[ -v FAH_PASSKEY ]]; then
+        FAH_CURRENT_PASSKEY=$(.local/bin/lufah -a / config passkey)
+        if [[ $FAH_CURRENT_PASSKEY != "\"$FAH_PASSKEY\"" ]]
+        then
+            echo "FAH passkey specified.  Updating FAH config"
+            .local/bin/lufah -a / config passkey $FAH_PASSKEY
+            echo "---"
+        fi
+    fi
 
-if [[ -v FAH_AUTOSTART && $FAH_AUTOSTART = "true" ]]; then
-    echo "FAH autostart enabled.  Folding is starting."
-    .local/bin/lufah -a / fold
-else
-    echo "FAH autostart disabled.  You will need to manually start folding on this machine"
-fi
+
+    if [[ -v FAH_AUTOSTART && $FAH_AUTOSTART = "true" ]]; then
+        echo "FAH autostart enabled."
+
+        FAH_CURRENT_USERNAME=$(.local/bin/lufah -a / config user)
+        if [[ -v FAH_USERNAME && $FAH_CURRENT_USERNAME != "\"$FAH_USERNAME\"" ]]
+        then
+            echo "Configured user ($FAH_CURRENT_USERNAME) does not match the specified user.  Will retry configuration"
+            echo "---"
+            echo "---"
+            sleep 1
+            continue;
+        fi
+        
+        FAH_CURRENT_TEAM=$(.local/bin/lufah -a / config team)
+        if [[ -v FAH_TEAM && $FAH_CURRENT_TEAM != "$FAH_TEAM" ]]
+        then
+            echo "Configured team ($FAH_CURRENT_TEAM) does not match the specified team.  Will retry configuration"
+            echo "---"
+            echo "---"
+            sleep 1
+            continue;
+        fi
+        
+        FAH_CURRENT_PASSKEY=$(.local/bin/lufah -a / config passkey)
+        if [[ -v FAH_PASSKEY && $FAH_CURRENT_PASSKEY != "\"$FAH_PASSKEY\"" ]]
+        then
+            echo "Configured passkey ($FAH_CURRENT_PASSKEY) does not match the specified passkey.  Will retry configuration"
+            echo "---"
+            echo "---"
+            sleep 1
+            continue;
+        fi
+
+        echo "Configuration finished.  Folding starting"
+        .local/bin/lufah -a / fold
+
+        # we only want to start folding once.  after one time, we enter the loop to ensure
+        # the configuration is still accurate (not overridden by account settings)
+        FAH_AUTOSTART=false
+    fi
+
+    # Sleep 5 seconds and then double check the config is accuate still
+    sleep 5
+done
